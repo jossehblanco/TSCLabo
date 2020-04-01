87ab0d5b31a0c7650acc4b8c2b5989e8b671c9f4
jossehblanco <jossehblanco@gmail.com>
2020-03-31 23:29:12 -0600

    Labo 2

--- main.cpp
+++ main.cpp
@@ -1,26 +1,51 @@
+#include <fstream>
 #include <iostream>
-#include "math_tools.h"
-#include "display_tools.h"
-#include <cstdlib>
-
+#include <string>
+#include "clases.h"
 using namespace std;
+
+
+
+void readCoordinates(ifstream &file, int n, item* item_list){
+    int e; float r;
+    for(int i=0; i<n; i++){
+        file >> e >> r;
+        item_list[i].setIntFloat(e,r);
+    }
+}
+
+
 int main() {
+    char filename[12]; //Se cambió al tamaño correcto porque corrompía el stack
+    string line;
+    mesh m;
+    ifstream file;
+    float k,Q;
+    int nnodes,neltos,ndirich,nneu;
 
-    //inicializando matriz de ejemplo
-    Matrix example_matrix;
-    zeroes(example_matrix, 3);
+    do{
+        cout << "Ingrese el nombre del archivo:" << endl;
+        cin >>filename;
 
-    example_matrix.at(0).at(0) = 2; example_matrix.at(0).at(1) = 2; example_matrix.at(0).at(2) = 3;
-    example_matrix.at(1).at(0) = 4; example_matrix.at(1).at(1) = 5; example_matrix.at(1).at(2) = 6;
-    example_matrix.at(2).at(0) = 7; example_matrix.at(2).at(1) = 8; example_matrix.at(2).at(2) = 9;
-    int det = determinant(example_matrix);
-    if(det != 0 ){
-        Matrix inverted;
-        invert(example_matrix, inverted);
-        showMatrix(inverted);
-    }else{
-        cout << "ERROR: Determinant cannot be zero. Unable to calculate inverse matrix." << endl;
-        return EXIT_FAILURE;
+        file.open(filename);
+
+    }while(!file);
+    file >> k >> Q;
+    file >> nnodes >> neltos >> ndirich >> nneu;
+
+    file >> line;
+
+    m.setParameters(k, Q);
+    m.setSizes(nnodes, neltos, ndirich, nneu);
+    m.createData();
+
+    readCoordinates(file,nnodes,m.getNodes());
+
+    file.close();
+
+    //Mostrando nodos a partir del objeto m
+    for(int i = 0; i < m.getSize(NODES); i++){
+        cout << m.getNodes()[i].getX() << endl;
     }
     return 0;
 }
